c70b5e7abef2e1be3e703d913201c5d63930e99b
Robert Raušel <robert.rausel@ingemark.com>
2019-12-03 08:15:35 +0100

    added path ambiguity error formatting

--- src/main/AsciiMapErrorFormatter.kt
+++ src/main/AsciiMapErrorFormatter.kt
@@ -5,6 +5,7 @@ object AsciiMapErrorFormatter {
     private const val CHARACTER_PLACEHOLDER = "{character}"
     private const val POSITION_PLACEHOLDER = "{position}"
     private const val PATH_BREAKS_ERROR_MESSAGE = "The path breaks on character [$CHARACTER_PLACEHOLDER] at position [$POSITION_PLACEHOLDER] because no adjacent items are part of a valid path."
+    private const val PATH_AMBIGUITY_ERROR_MESSAGE = "The path breaks on character [$CHARACTER_PLACEHOLDER] at position [$POSITION_PLACEHOLDER] because adjacent items describe an ambiguous path."
 
     const val EMPTY_INPUT_ERROR_MESSAGE = "Input cannot be empty"
     const val NO_START_CHARACTER_ERROR_MESSAGE = "Input must have exactly one start character marked with \"@\"."
@@ -12,6 +13,11 @@ object AsciiMapErrorFormatter {
 
     fun formatPathBreakError(asciiMapItem: AsciiMapItem): String {
         val replacedCharacter = PATH_BREAKS_ERROR_MESSAGE.replace(CHARACTER_PLACEHOLDER, asciiMapItem.character)
+        return replacedCharacter.replace(POSITION_PLACEHOLDER, "${asciiMapItem.rowIndex}, ${asciiMapItem.columnIndex}")
+    }
+
+    fun formatPathAmbiguityError(asciiMapItem: AsciiMapItem): String {
+        val replacedCharacter = PATH_AMBIGUITY_ERROR_MESSAGE.replace(CHARACTER_PLACEHOLDER, asciiMapItem.character)
         return replacedCharacter.replace(POSITION_PLACEHOLDER, "${asciiMapItem.rowIndex}, ${asciiMapItem.columnIndex}")
     }
 }
