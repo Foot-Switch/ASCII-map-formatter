5233b5dc7d99044dff216244f31916d6208e229f
Robert Raušel <robert.rausel@ingemark.com>
2019-12-05 07:29:41 +0100

    code reduction

--- src/test/AsciiMapErrorFormatterTest.kt
+++ src/test/AsciiMapErrorFormatterTest.kt
@@ -6,16 +6,16 @@ import org.junit.Assert.*
 
 class AsciiMapErrorFormatterTest {
 
+    private val asciiMapItem = AsciiMapItem("a", 0, 0)
+
     @Test
     fun formatPathBreakError() {
-        val asciiMapItem = AsciiMapItem("a", 0, 0)
         val errorMessage = AsciiMapErrorFormatter.formatPathBreakErrorMessage(asciiMapItem)
         assertEquals("The path breaks on character [${asciiMapItem.character}] at position [${asciiMapItem.rowIndex}, ${asciiMapItem.columnIndex}] because no adjacent items are part of a valid path.", errorMessage)
     }
 
     @Test
     fun formatPathAmbiguityError() {
-        val asciiMapItem = AsciiMapItem("a", 0, 0)
         val errorMessage = AsciiMapErrorFormatter.formatPathAmbiguityErrorMessage(asciiMapItem)
         assertEquals("The path breaks on character [${asciiMapItem.character}] at position [${asciiMapItem.rowIndex}, ${asciiMapItem.columnIndex}] because adjacent items describe an ambiguous path.", errorMessage)
     }
