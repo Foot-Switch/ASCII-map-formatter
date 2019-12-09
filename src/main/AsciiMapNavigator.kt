493e708e60431c0b1b386e4743647a1e31920829
Robert Raušel <robert.rausel@ingemark.com>
2019-12-09 09:29:53 +0100

    moved helper methods to navigator, pending tests

---
+++ src/main/AsciiMapNavigator.kt
@@ -0,0 +1,134 @@
+package main
+
+object AsciiMapNavigator {
+
+    const val startCharacter = "@"
+    const val endCharacter = "x"
+    const val pathCharacterHorizontal = "-"
+    const val pathCharacterVertical = "|"
+    const val pathCharacterCorner = "+"
+
+    private const val unambiguousNumberOfNextItemCandidates = 1
+
+    private var allItems: List<AsciiMapItem>? = null
+    private var pathItems: MutableList<AsciiMapItem>? = null
+
+    fun assignMap(asciiMapInput: String) {
+        allItems = AsciiMapItemSerializer.serializeAsciiMapItems(asciiMapInput)
+        pathItems = mutableListOf()
+    }
+
+    fun findNextItem(allMapItems: List<AsciiMapItem>?, previousItem: AsciiMapItem?, currentItem: AsciiMapItem): AsciiMapItem? {
+        val nextItem: AsciiMapItem?
+        val allAdjacentItems = findAdjacentItems(allMapItems, currentItem)
+        val validAdjacentItems = removeNonPathItems(allAdjacentItems)
+        removePreviousItemFromValidAdjacentItems(previousItem, validAdjacentItems)
+        nextItem = when {
+            validAdjacentItems.isEmpty() -> throw Exception(AsciiMapErrorFormatter.formatPathBreakErrorMessage(currentItem))
+            startIsAmbiguous(currentItem, validAdjacentItems) -> throw Exception(AsciiMapErrorFormatter.formatPathAmbiguityErrorMessage(currentItem))
+            isJunction(validAdjacentItems) -> findNextItemInJunction(previousItem!!, currentItem, validAdjacentItems)
+            else -> getTheOnlyRemainingNextItemCandidate(validAdjacentItems)
+        }
+        return nextItem
+    }
+
+    fun findAdjacentItems(allMapItems: List<AsciiMapItem>?, currentItem: AsciiMapItem): MutableList<AsciiMapItem?> {
+        val leftItem = allMapItems?.find { it.rowIndex == currentItem.rowIndex && it.columnIndex == currentItem.columnIndex - 1 }
+        val topItem = allMapItems?.find { it.rowIndex == currentItem.rowIndex - 1 && it.columnIndex == currentItem.columnIndex }
+        val rightItem = allMapItems?.find { it.rowIndex == currentItem.rowIndex && it.columnIndex == currentItem.columnIndex + 1 }
+        val bottomItem = allMapItems?.find { it.rowIndex == currentItem.rowIndex + 1 && it.columnIndex == currentItem.columnIndex }
+        return mutableListOf(leftItem, topItem, rightItem, bottomItem)
+    }
+
+    fun removeNonPathItems(allAdjacentItems: List<AsciiMapItem?>): MutableList<AsciiMapItem> {
+        val nextItemCandidates = mutableListOf<AsciiMapItem>()
+        allAdjacentItems.forEach { adjacentItem -> if (isPathItem(adjacentItem)) nextItemCandidates.add(adjacentItem!!) }
+        return nextItemCandidates
+    }
+
+    fun removePreviousItemFromValidAdjacentItems(previousItem: AsciiMapItem?, validAdjacentItems: MutableList<AsciiMapItem>) {
+        validAdjacentItems.removeIf { itemsHaveSamePosition(it, previousItem) }
+    }
+
+    fun startIsAmbiguous(currentItem: AsciiMapItem, nextItemCandidates: List<AsciiMapItem>): Boolean {
+        return currentItem.character == startCharacter && nextItemCandidates.size > unambiguousNumberOfNextItemCandidates
+    }
+
+    fun isJunction(nextItemCandidates: List<AsciiMapItem>) = nextItemCandidates.size > unambiguousNumberOfNextItemCandidates
+
+    fun itemsHaveSamePosition(itemOne: AsciiMapItem?, itemTwo: AsciiMapItem?) =
+            itemOne?.rowIndex == itemTwo?.rowIndex && itemOne?.columnIndex == itemTwo?.columnIndex
+
+    fun findNextItemInJunction(previousItem: AsciiMapItem, currentItem: AsciiMapItem, nextItemCandidates: List<AsciiMapItem>): AsciiMapItem {
+        nextItemCandidates.find { it.character == endCharacter }?.let { return it }
+        return if (enteredHorizontally(previousItem, currentItem))
+            findNextHorizontalItem(previousItem, currentItem, nextItemCandidates)?.let { it }
+                    ?: throw Exception(AsciiMapErrorFormatter.formatPathAmbiguityErrorMessage(currentItem))
+        else
+            findNextVerticalItem(previousItem, currentItem, nextItemCandidates)?.let { it }
+                    ?: throw Exception(AsciiMapErrorFormatter.formatPathAmbiguityErrorMessage(currentItem))
+    }
+
+    fun enteredHorizontally(previousItem: AsciiMapItem, currentItem: AsciiMapItem) =
+            currentItem.rowIndex == previousItem.rowIndex
+
+    fun findNextHorizontalItem(previousItem: AsciiMapItem, currentItem: AsciiMapItem, nextItemCandidates: List<AsciiMapItem>): AsciiMapItem? {
+        val nextHorizontalPosition = if (previousItem.columnIndex < currentItem.columnIndex) currentItem.columnIndex + 1 else currentItem.columnIndex - 1
+        return nextItemCandidates.find { it.columnIndex == nextHorizontalPosition }
+    }
+
+    fun findNextVerticalItem(previousItem: AsciiMapItem, currentItem: AsciiMapItem, nextItemCandidates: List<AsciiMapItem>): AsciiMapItem? {
+        val nextVerticalPosition = if (previousItem.rowIndex < currentItem.rowIndex) currentItem.rowIndex + 1 else currentItem.rowIndex - 1
+        return nextItemCandidates.find { it.rowIndex == nextVerticalPosition }
+    }
+
+    fun getTheOnlyRemainingNextItemCandidate(nextItemCandidates: List<AsciiMapItem>) = nextItemCandidates[0]
+
+    fun isPathItem(asciiMapItem: AsciiMapItem?) =
+            asciiMapItem != null &&
+                    (asciiMapItem.character == pathCharacterHorizontal
+                            || asciiMapItem.character == pathCharacterVertical
+                            || asciiMapItem.character.single().isLetter()
+                            || asciiMapItem.character == pathCharacterCorner
+                            || asciiMapItem.character == endCharacter)
+
+    fun buildItemPath() {
+        val startItems = allItems?.filter { it.character == startCharacter }
+        val endItems = allItems?.filter { it.character == endCharacter }
+        when {
+            startItems?.size != 1 -> throw Exception(AsciiMapErrorFormatter.START_CHARACTER_ERROR_MESSAGE)
+            endItems?.size != 1 -> throw Exception(AsciiMapErrorFormatter.END_CHARACTER_ERROR_MESSAGE)
+            else -> addNextItemToPath(null, startItems[0])
+        }
+    }
+
+    fun addNextItemToPath(previousItem: AsciiMapItem?, currentItem: AsciiMapItem) {
+        pathItems?.add(currentItem)
+        if (currentItem.character == endCharacter) return
+        val nextItem = findNextItem(allItems, previousItem, currentItem) ?: return
+        addNextItemToPath(currentItem, nextItem)
+    }
+
+
+    fun formatOutputFromItemPath(): AsciiMapOutput {
+        val letterItems = mutableListOf<AsciiMapItem>()
+        var pathAsCharacters = ""
+        pathItems?.forEach { asciiMapItem ->
+            pathAsCharacters += asciiMapItem.character
+            if (isPathLetterCharacter(asciiMapItem) && !letterItems.contains(asciiMapItem))
+                letterItems.add(asciiMapItem)
+        }
+        var letters = ""
+        letterItems.forEach { asciiMapItem -> letters += asciiMapItem.character }
+        if (letters.isBlank()) letters = AsciiMapErrorFormatter.NO_LETTERS_MESSAGE
+        return AsciiMapOutput(letters, pathAsCharacters)
+    }
+
+    fun isPathLetterCharacter(asciiMapItem: AsciiMapItem) =
+            asciiMapItem.character.single().isLetter() && asciiMapItem.character != endCharacter
+
+    fun buildOutput(): AsciiMapOutput {
+        buildItemPath()
+        return formatOutputFromItemPath()
+    }
+}
--- src/test/AsciiMapTest.kt
+++ src/test/AsciiMapTest.kt
@@ -17,66 +17,73 @@ class AsciiMapTest {
     @Test
     fun throwExceptionWhenNoStartCharacterIsPresent() {
         exceptionRule.expectMessage(START_CHARACTER_ERROR_MESSAGE)
-        AsciiMap(AsciiMapTestData.mapWithoutStart)
+        val asciiMap = AsciiMap(AsciiMapTestData.mapWithoutStart)
+        asciiMap.getOutput()
     }
 
     @Test
     fun throwExceptionWhenNoEndCharacterIsPresent() {
         exceptionRule.expectMessage(END_CHARACTER_ERROR_MESSAGE)
-        AsciiMap(AsciiMapTestData.mapWithoutEnd)
+        val asciiMap = AsciiMap(AsciiMapTestData.mapWithoutEnd)
+        asciiMap.getOutput()
     }
 
     @Test
     fun throwExceptionWhenMultipleStartCharactersArePresent() {
         exceptionRule.expectMessage(START_CHARACTER_ERROR_MESSAGE)
-        AsciiMap(AsciiMapTestData.mapWithMultipleStart)
+        val asciiMap = AsciiMap(AsciiMapTestData.mapWithMultipleStart)
+        asciiMap.getOutput()
     }
 
     @Test
     fun throwExceptionWhenMultipleEndCharactersArePresent() {
         exceptionRule.expectMessage(END_CHARACTER_ERROR_MESSAGE)
-        AsciiMap(AsciiMapTestData.mapWithMultipleEnd)
+        val asciiMap = AsciiMap(AsciiMapTestData.mapWithMultipleEnd)
+        asciiMap.getOutput()
     }
 
     @Test
     fun throwExceptionWhenPathBreaks() {
         exceptionRule.expectMessage(formatPathBreakErrorMessage(AsciiMapItem("@", 0, 0)))
-        AsciiMap(AsciiMapTestData.brokenPathMap)
+        val asciiMap = AsciiMap(AsciiMapTestData.brokenPathMap)
+        asciiMap.getOutput()
     }
 
     @Test
     fun throwExceptionWhenStartIsAmbiguous() {
         exceptionRule.expectMessage(formatPathAmbiguityErrorMessage(AsciiMapItem("@", 0, 2)))
-        AsciiMap(AsciiMapTestData.ambiguousStartMap)
+        val asciiMap = AsciiMap(AsciiMapTestData.ambiguousStartMap)
+        asciiMap.getOutput()
     }
 
     @Test
     fun throwExceptionWhenJunctionIsAmbiguous() {
         exceptionRule.expectMessage(formatPathAmbiguityErrorMessage(AsciiMapItem("+", 1, 8)))
-        AsciiMap(AsciiMapTestData.ambiguousJunctionMap)
+        val asciiMap = AsciiMap(AsciiMapTestData.ambiguousJunctionMap)
+        asciiMap.getOutput()
     }
 
     @Test
     fun formatOutputWithoutLetters() {
         val asciiMap = AsciiMap(AsciiMapTestData.mapWithoutLetters)
-        assertEquals(AsciiMapTestData.expectedOutputWithoutLetters, asciiMap.output)
+        assertEquals(AsciiMapTestData.expectedOutputWithoutLetters, asciiMap.getOutput())
     }
 
     @Test
     fun formatOutputOne() {
         val asciiMap = AsciiMap(AsciiMapTestData.mapOne)
-        assertEquals(AsciiMapTestData.expectedOutputOne, asciiMap.output)
+        assertEquals(AsciiMapTestData.expectedOutputOne, asciiMap.getOutput())
     }
 
     @Test
     fun formatOutputTwo() {
         val asciiMap = AsciiMap(AsciiMapTestData.mapTwo)
-        assertEquals(AsciiMapTestData.expectedOutputTwo, asciiMap.output)
+        assertEquals(AsciiMapTestData.expectedOutputTwo, asciiMap.getOutput())
     }
 
     @Test
     fun formatOutputThree() {
         val asciiMap = AsciiMap(AsciiMapTestData.mapThree)
-        assertEquals(AsciiMapTestData.expectedOutputThree, asciiMap.output)
+        assertEquals(AsciiMapTestData.expectedOutputThree, asciiMap.getOutput())
     }
 }
